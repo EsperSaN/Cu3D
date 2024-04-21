5cf831d495dc508919e41fe2b8dac496fe2ec6d2
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-04-21 14:48:23 +0700

    fix the abs

--- src/util/abs_deg.c
+++ src/util/abs_deg.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   abs_dregree.c                                      :+:      :+:    :+:   */
+/*   abs_deg.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 02:17:46 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/04/12 02:28:14 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/04/21 14:47:24 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 float	abs_deg(float dregree)
 {
-	if (dregree == 360)
+	if (dregree > 359.9)
 		return (0);
-	if (dregree == 0)
+	if (dregree < 0)
 		return (360);
 	return (dregree);
 }
