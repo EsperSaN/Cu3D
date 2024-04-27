119694be302cc5e68bc07ed3e8f65131a2f06866
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-04-28 00:54:21 +0700

    change the init to heap and calloc

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 21:25:08 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/04/21 22:00:53 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/04/28 00:53:52 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,13 @@
 
 int	main(int ac, char **av)
 {
-	t_data	d;
+	t_data	*d;
+
 	if (ac != 2)
 		return (puts(WRG_ARG_NO), 1);
-
+	d = ft_calloc(sizeof(t_data), 1);
+	if (d == NULL)
+		return (1);
 	(void)av;
-	d.parser_data = main_parser(av[1]);
-	//d.maps = get_data_from_parser(d.parser_data);
-	d.mlx = mlx_init(SCREEN_WIDTH, SCREEN_HIGHT, "ray", false);
-	
-	d.img_game = mlx_new_image(d.mlx, SCREEN_WIDTH / 2, SCREEN_HIGHT);
-	d.img_maps = mlx_new_image(d.mlx, SCREEN_WIDTH / 2, SCREEN_HIGHT);
-	
-	mlx_image_to_window(d.mlx, d.img_maps, 0, 0);
-	mlx_image_to_window(d.mlx, d.img_game, SCREEN_WIDTH / 2, 0);
-	
-	clear_image(d.img_game, get_rgba(255, 0, 0, 255));
-	clear_image(d.img_maps, get_rgba(0, 255, 0, 255));
-	mlx_loop(d.mlx);
-	puts("done");
-	// d = init();
-	// d->parser_data = data_parser(av); // do the read file
-	// assign_data(d);
-
+	d->parser_data = main_parser(av[1]);
 }
