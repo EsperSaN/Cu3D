32b939b6fea850f2d72432642c8c8b84118aed22
EsperSaN <polawat.ruenruay@hotmail.com>
2024-05-27 00:50:46 +0700

    change the cos to ftcos

--- src/util/rotate_vector.c
+++ src/util/rotate_vector.c
@@ -2,13 +2,11 @@
 
 t_float_point rotate_vector(t_float_point vec, float angle)
 {
-    float radian;
     t_float_point tmp;
 
-    radian = get_rad(angle);
     tmp.x = vec.x;
     tmp.y = vec.y;
-    vec.x = tmp.x * cosf(radian) - tmp.y * sinf(radian);
-    vec.y = tmp.x * sin(radian) + tmp.y * cosf(radian);
+    vec.x = tmp.x * ft_cos(angle) - tmp.y * ft_sin(angle);
+    vec.y = tmp.x * ft_sin(angle) + tmp.y * ft_cos(angle);
     return (vec);
 }
