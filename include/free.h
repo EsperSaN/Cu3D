faf53c2b6634c0fbae3a285ce43b411e26fdcf82
EsperSaN <polawat.ruenruay@hotmail.com>
2024-06-04 15:32:43 +0700

    fix norm

--- include/free.h
+++ include/free.h
@@ -6,7 +6,7 @@
 /*   By: wave <wave@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 17:34:25 by wave              #+#    #+#             */
-/*   Updated: 2024/06/02 17:34:58 by wave             ###   ########.fr       */
+/*   Updated: 2024/06/04 15:03:07 by wave             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "cube.h"
 
 t_texture			*free_texture(t_texture *data);
+t_texture			*free_texture2(t_texture *data, int i);
 t_texture_assets	*free_texture_assets(t_texture_assets *data);
 t_player_data		*free_player(t_player_data *data);
 t_parser_data		*free_parser(t_parser_data *data);
