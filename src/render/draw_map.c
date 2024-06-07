bd967b5906eea014ad4055979246cbbfa7e8889d
Polawat Ruenruay <pruenrua@z1t4c4.42bangkok.com>
2024-06-07 17:50:10 +0700

    fix draw maps

--- src/render/draw_map.c
+++ src/render/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:24:31 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/06/07 14:54:27 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/06/07 17:48:35 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,36 +25,38 @@ static	void	draw_player(mlx_image_t *image, t_int_point pos, int scale \
 	draw_line(image, pos, dir, get_rgba(0, 255, 255, 255));
 }
 
+void	put_maps(t_dm m, mlx_image_t *image, t_maps_data *maps_data)
+{
+	while (m.cur_p.x < 20 && m.pos.y >= 0)
+	{
+		if (m.pos.x >= 0 && m.pos.y >= 0 && m.pos.y < maps_data->maps_height && \
+		m.pos.x < (int)(ft_strlen(maps_data->maps_array[m.pos.y])) && \
+		maps_data->maps_array[m.pos.y][m.pos.x] == WALL)
+			draw_square(image, m.draw, m.scale, get_rgba(255, 0, 0, 255));
+		m.pos.x++;
+		m.draw.x += m.scale;
+		m.cur_p.x++;
+	}
+}
+
 void	draw_maps(t_data *d, mlx_image_t *image, t_maps_data *maps_data)
 {
-	t_int_point	pos;
-	t_int_point	cur_p;
-	t_int_point	draw;
-	int			scale;
+	t_dm	m;
 
-	cur_p.y = 0;
-	scale = image->width / 20;
-	pos.y = (int)(d->player->pos.y) - 10;
-	draw.y = 0;
-	while (cur_p.y < 20)
+	m.cur_p.y = 0;
+	m.scale = image->width / 20;
+	m.pos.y = (int)(d->player->pos.y) - 10;
+	m.draw.y = 0;
+	while (m.cur_p.y < 20)
 	{
-		cur_p.x = 0;
-		pos.x = (int)(d->player->pos.x) - 10;
-		draw.x = 0;
-		while (cur_p.x < 20 && pos.y >= 0)
-		{
-			if (pos.x >= 0 && pos.y >= 0 && \
-			pos.x < maps_data->maps_width && pos.y < maps_data->maps_height && \
-			maps_data->maps_array[pos.y][pos.x] && maps_data->maps_array[pos.y][pos.x] == WALL)
-				draw_square(image, draw, scale, get_rgba(255, 0, 0, 255));
-			pos.x++;
-			draw.x += scale;
-			cur_p.x++;
-		}
-		draw.y += scale;
-		pos.y++;
-		cur_p.y++;
+		m.cur_p.x = 0;
+		m.pos.x = (int)(d->player->pos.x) - 10;
+		m.draw.x = 0;
+		put_maps(m, image, maps_data);
+		m.draw.y += m.scale;
+		m.pos.y++;
+		m.cur_p.y++;
 	}
-	draw_player(image, pos, scale, d->player->dir);
-	return;
+	draw_player(image, m.pos, m.scale, d->player->dir);
+	return ;
 }
