b257b3318c4e198456f5551633a2bc3a191c3114
EsperSaN <polawat.ruenruay@hotmail.com>
2024-06-12 02:48:07 +0700

    check_resourse : fix norm

--- src/parser/check_resource.c
+++ src/parser/check_resource.c
@@ -3,27 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   check_resource.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pruenrua <pruenrua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: wave <wave@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 16:28:22 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/06/11 00:49:40 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/06/12 02:46:52 by wave             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-int	check_resource(char **map, t_parser_data *res)
+void	get_resource(char **map, t_parser_data *res, int *elm_count)
 {
 	int		i;
-	int		elm_count;
 	char	*element[3];
 	char	*tmp;
 	int		size_to_get;
 
 	element[2] = NULL;
-	elm_count = 0;
 	i = 0;
-	dprintf(2, "\n\n\ncheck_resource\n");
 	while (map[i] && is_map_line(map[i]) != -1)
 	{
 		element[0] = ft_strtrim(map[i], " cn\t\r\v\f");
@@ -35,11 +32,19 @@ int	check_resource(char **map, t_parser_data *res)
 		tmp = element[1];
 		element[1] = ft_strtrim(element[1], " \n\t\r\v\f");
 		free(tmp);
-		elm_count += get_element_check(element[0], element[1], res);
+		*elm_count += get_element_check(element[0], element[1], res);
 		free(element[0]);
 		free(element[1]);
 		i++;
 	}
+}
+
+int	check_resource(char **map, t_parser_data *res)
+{
+	int	elm_count;
+
+	elm_count = 0;
+	get_resource(map, res, &elm_count);
 	if (elm_count != 6)
 		return (puterror("Map element are not good"), false);
 	return (true);
