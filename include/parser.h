ad74e0834a6446b16ba309e21a300ac7826e4d8e
EsperSaN <polawat.ruenruay@hotmail.com>
2024-06-12 18:31:00 +0700

    fix unuse

--- include/parser.h
+++ include/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pruenrua <pruenrua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 22:14:51 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/06/11 00:48:01 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/06/12 14:41:07 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "cube.h"
 # define BUFFER_SIZE 1
 
-char				**init_map(char **smap, int width, int height);
+char				**init_map(char **smap, int height);
 void				print_map(char **map);
 void				print_map_data(t_parser_data *res);
 char				**file_reader(int fd);
